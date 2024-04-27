39991d574450a21b7428c7836f836d155d1c8bc7
shahadat <nayemshahadat581@gmail.com>
2024-04-27 18:07:51 +0600

    get all data done

--- index.js
+++ index.js
@@ -23,14 +23,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const spotCollection=client.db('spotDB').collection('spot')
+
+    app.get('/spot', async (req,res)=>{
+        const cursor=spotCollection.find();
+        const result=await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/spot', async (req,res)=>{
+            const spot=req.body;
+            const result= await spotCollection.insertOne(spot);
+             res.send(result);
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+   
+
+    
   }
 }
 run().catch(console.dir);
