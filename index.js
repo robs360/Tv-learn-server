6a7fef0bdd309f9379aceb1257c6a974d507941d
shahadat <nayemshahadat581@gmail.com>
2024-04-27 19:32:46 +0600

    single data get done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const cors=require("cors");
 const app=express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =process.env.PORT || 5000;
 
 app.use(cors());
@@ -32,7 +32,13 @@ async function run() {
         const result=await cursor.toArray();
         res.send(result);
     })
-
+  
+    app.get('/spot/:id',async (req,res)=>{
+       const id=req.params.id;
+       const query= {_id: new ObjectId(id)};
+       const result=await spotCollection.findOne(query);
+       res.send(result)
+    })
     app.post('/spot', async (req,res)=>{
             const spot=req.body;
             const result= await spotCollection.insertOne(spot);
